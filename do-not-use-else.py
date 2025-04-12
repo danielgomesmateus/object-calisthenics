7966e520b15c97bcef3f48075f45ef818fabea14
Daniel Gomes Mateus <daniel.mateus@syngenta.com>
2025-04-12 13:10:08 -0300

    feat: add an example

--- do-not-use-else.py
+++ do-not-use-else.py
@@ -5,7 +5,6 @@ def login(request):
     if request.username == username and request.password == password:
         redirect("homepage")
     else:
-        add_flash("error", "Bad credentials")
         redirect("login")
 
 
@@ -15,6 +14,4 @@ def login(request):
 
     if request.username == username and request.password == password:
         return redirect("homepage")
-
-    add_flash("error", "Bad credentials")
     return redirect("login")
